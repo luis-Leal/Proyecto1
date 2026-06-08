d69cdf51f7f7dcc8a6cb5663859c53291e83bd2d
Alejandro <luisalejandroleal@outlook.com>
2026-06-08 12:13:23 -0500

    Fix puerto para Railway

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.post('/api/login', (req, res) => {
     res.json({ mensaje: 'Login exitoso', token });
 });
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
